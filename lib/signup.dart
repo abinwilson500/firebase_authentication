6ce0b82dd6b1d1b47bae18cb5b30433b0cdb63a1
abinwilson500 <abinwilson500@gmail.com>
2024-11-14 09:21:56 +0530

    authentication

---
+++ lib/signup.dart
@@ -0,0 +1,177 @@
+import 'package:file_authentication/forgetpassword.dart';
+import 'package:file_authentication/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:file_authentication/forgetpassword.dart';
+import 'package:file_authentication/login.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class Signup extends StatefulWidget {
+  const Signup({super.key});
+  @override
+  State<Signup> createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  Future<UserCredential?> loginwithGoogle() async {
+    try {
+      final GoogleSignIn googleSignIn = GoogleSignIn(
+        clientId:
+            "414069224916-57iskmrbquuvq0a1b2qiobr1q37t8lgr.apps.googleusercontent.com",
+      );
+      final googleuser = await googleSignIn.signIn();
+      final googleAuth = await googleuser?.authentication;
+      final cred = GoogleAuthProvider.credential(
+          idToken: googleAuth?.idToken, accessToken: googleAuth?.accessToken);
+      return await FirebaseAuth.instance.signInWithCredential(cred);
+    } catch (e) {
+      print(e.toString());
+    }
+    return null;
+  }
+
+  Future<void> _signUp() async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+      );
+      print('User signed up');
+    } catch (e) {
+      print('Sign-up error: $e');
+      _showErrorDialog(e.toString());
+    }
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Error'),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 400,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/download (2).jpeg"),
+                    fit: BoxFit.cover)),
+          ),
+          SizedBox(height: 20),
+          SizedBox(
+            height: 40,
+            width: 300,
+            child: TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15))),
+            ),
+          ),
+          SizedBox(height: 20),
+          SizedBox(
+            height: 40,
+            width: 300,
+            child: TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15)))),
+          ),
+          SizedBox(height: 55),
+          SizedBox(
+            height: 40,
+            width: 300,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orange[600],
+                  foregroundColor: Colors.white),
+              onPressed: () {
+                _signUp();
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => LoginPage()));
+              },
+              child: Text('Sign Up', style: TextStyle(fontSize: 18)),
+            ),
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Already have an Account?",
+                style: TextStyle(color: Colors.black, fontSize: 18),
+              ),
+              SizedBox(width: 10),
+              GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => LoginPage()));
+                  },
+                  child: Text(
+                    "Login",
+                    style: TextStyle(color: Colors.orange, fontSize: 18),
+                  ))
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ForgotPasswordScreen()));
+                  },
+                  child: Text(
+                    "Forgot password?",
+                    style: TextStyle(fontSize: 18, color: Colors.blue),
+                  )),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("sigin with"),
+                  SizedBox(width: 5),
+                  GestureDetector(
+                    onTap: () async {
+                      await loginwithGoogle();
+                    },
+                    child: Container(
+                      height: 20,
+                      width: 20,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage("assets/images/download.png"))),
+                    ),
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
